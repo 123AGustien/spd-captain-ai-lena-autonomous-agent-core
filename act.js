d8d9f5029a22e94397e3169a903e6d8d80890c4e
Mr.Don Herman Oswald Weerasekera <donweerasekera@gmail.com>
2026-07-06 02:35:22 +0700

    Update act.js

--- act.js
+++ act.js
@@ -1,1 +1,32 @@
-g
+import { state } from "../state.js";
+
+export function ACT(decision) {
+  state.lastDecision = decision;
+
+  switch (decision) {
+    case "IDLE":
+      state.risk = "LOW";
+      state.mode = "IDLE";
+      break;
+
+    case "LOW_ACTION":
+      state.ENERGY += 1;
+      state.risk = "LOW";
+      state.mode = "MONITOR";
+      break;
+
+    case "MEDIUM_ACTION":
+      state.FX += 1;
+      state.ENERGY += 1;
+      state.risk = "MEDIUM";
+      state.mode = "ADJUST";
+      break;
+
+    case "HIGH_ACTION":
+      state.CYB += 1;
+      state.INF += 1;
+      state.risk = "HIGH";
+      state.mode = "INTERVENTION";
+      break;
+  }
+}
