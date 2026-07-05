5dc0b168223c3a14529d14a5ad8ad461fb637c56
Mr.Don Herman Oswald Weerasekera <donweerasekera@gmail.com>
2026-07-06 02:31:50 +0700

    Update state.js

--- state.js
+++ state.js
@@ -1,1 +1,14 @@
-g
+export const state = {
+  FX: 0,
+  ENERGY: 0,
+  CYB: 0,
+  INF: 0,
+
+  risk: "LOW",
+  mode: "IDLE",
+  running: false,
+
+  lastInput: null,
+  lastDecision: null,
+  cycleCount: 0
+};
