35e55e4a2eea05e9214ece8895539258310380d7
Mr.Don Herman Oswald Weerasekera <donweerasekera@gmail.com>
2026-07-06 02:32:49 +0700

    Update observe.js

--- observe.js
+++ observe.js
@@ -1,1 +1,8 @@
-g
+export function OBSERVE(input) {
+  return {
+    FX: input.includes("FX") ? 1 : 0,
+    ENERGY: input.includes("ENERGY") ? 1 : 0,
+    CYB: input.includes("CYB") ? 1 : 0,
+    INF: input.includes("INF") ? 1 : 0
+  };
+}
