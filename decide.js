5464555626f8791fa33451244ea8a13cecff3750
Mr.Don Herman Oswald Weerasekera <donweerasekera@gmail.com>
2026-07-06 02:34:34 +0700

    Update decide.js

--- decide.js
+++ decide.js
@@ -1,1 +1,12 @@
-g
+export function DECIDE(obs) {
+  const score =
+    obs.FX +
+    obs.ENERGY +
+    obs.CYB +
+    obs.INF;
+
+  if (score === 0) return "IDLE";
+  if (score <= 2) return "LOW_ACTION";
+  if (score <= 3) return "MEDIUM_ACTION";
+  return "HIGH_ACTION";
+}
