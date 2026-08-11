09d8766b0e05638782975e74e47c09b4445baa4a
Mr.Don Herman Oswald Weerasekera <donweerasekera@gmail.com>
2026-08-12 02:26:53 +0700

    Update fx.js

--- fx.js
+++ fx.js
@@ -1,5 +1,7 @@
-export function fxModule(fx) {
-  if (fx > 10) return "FX STABILIZATION REQUIRED";
-  if (fx < 0) return "FX UNDERFLOW WARNING";
-  return "FX STABLE";
+export function riskModule(cyb, energy, fx) {
+  const stress = (cyb + (100 - energy) + fx) / 3;
+
+  if (stress >= 70) return "HIGH RISK";
+  if (stress >= 40) return "MEDIUM RISK";
+  return "LOW RISK";
 }
