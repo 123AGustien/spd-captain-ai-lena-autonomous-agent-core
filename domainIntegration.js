7e99f3e29114ad001749d62c391ec6e0804f4e54
Mr.Don Herman Oswald Weerasekera <donweerasekera@gmail.com>
2026-08-31 23:35:43 +0700

    Remove unused domain engine imports
    Removed imports for various rule engines from domainIntegration.js.

--- domainIntegration.js
+++ domainIntegration.js
@@ -43,23 +43,7 @@
 
 
 /* =========================================================
-   DOMAIN ENGINE IMPORTS
-========================================================= */
-
-import * as FINRuleEngine
-  from "./FIN/finRuleEngine.js";
-
-import * as BHRRuleEngine
-  from "./BHR/BHRRuleEngine.js";
-
-import * as DCRuleEngine
-  from "./DC/DCRuleEngine.js";
-
-import * as CYBRuleEngine
-  from "./CYB/CYBRuleEngine.js";
-
-import * as INFRuleEngine
-  from "./INF/INFRuleEngine.js";
+kkkkkk
 
 
 /* =========================================================
